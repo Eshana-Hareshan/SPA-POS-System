a65ec7606b28d290d5a9a258c170543d42ef4823
Eshana-Hareshan <eshanahbandara@gmail.com>
2026-05-02 21:24:57 +0530

    create function

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -2,4 +2,7 @@ let userName = document.getElementById('user-name');
 let password = document.getElementById('user-password');
 let submitButton = document.getElementsByClassName('btn-submit');
 
-
+submitButton.addEventListener('click', function(){
+    userName.value = 'Eshana';
+    password.value = '123';
+})
