b274ab314830df51115b757f41d6a517065f4c27
Eshana-Hareshan <eshanahbandara@gmail.com>
2026-05-02 22:57:11 +0530

    function loggin page

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -1,8 +1,23 @@
 let userName = document.getElementById('user-name');
 let password = document.getElementById('user-password');
-let submitButton = document.getElementsByClassName('btn-submit');
+let submitButton = document.getElementById('submit-btn');
 
-submitButton.addEventListener('click', function(){
-    userName.value = 'Eshana';
-    password.value = '123';
-})
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const UserName = "Eshana";
+    const Password = "123";
+
+    if (userName.value === UserName) {
+        if (password.value === Password) {
+
+            $('#login-page').hide();
+            $('#customer_content').show();
+
+        } else {
+            alert("Password incorrect");
+        }
+    } else {
+        alert("Username incorrect");
+    }
+});
