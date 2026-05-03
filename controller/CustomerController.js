434ce514b70490830ed2266cd6f5cbcb978f8250
Eshana-Hareshan <eshanahbandara@gmail.com>
2026-05-04 01:24:25 +0530

    recreate customer CRUD

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -1,39 +1,98 @@
-// Customer Save
-document.getElementById('save_customer_btn').addEventListener('click', function () {
+import {addCustomerData, updateCustomerData, deleteCustomerData, getCustomerData, getCustomerDataByIndex, getCustomerDataById} from '../model/CustomerModel.js';
+import {check_nic, check_phone} from '../utils/regex_util.js';
 
-    const id = document.getElementById('customer_id').value;
-    const name = document.getElementById('customer_name').value;
-    const address = document.getElementById('customer_address').value;
-    const contact = document.getElementById('customer_contact').value;
+//------------------------- Load Customer Tbl (Read) ------------------------------
+const loadCustomerTbl = () => {
+    $('#customer_table_body').empty();
+    let customer_db = getCustomerData();
+    customer_db.map((item, index) => {
+        let new_row = `<tr data-index="${index}"> <td>${item.id}</td> <td>${item.name}</td> <td>${item.nic}</td> <td>${item.phone}</td> <td>${item.address}</td> </tr>`;
+        $('#customer_table_body').append(new_row);
+    });
+}
 
-    if (!id || !name || !address || !contact) {
-        alert("Please fill all fields");
-        return;
-    }
+//------------------------- Clean Customer Form ------------------------------
+const cleanCustomerForm = () => {
+    $('#customer_reset_btn').click();
+}
 
-    const newCustomer = `
-        <tr>
-            <td>${id}</td>
-            <td>${name}</td>
-            <td>${address}</td>
-            <td>${contact}</td>
-        </tr>
-    `;
-
-    document.getElementById('customer_table_body').innerHTML += newCustomer;
-
-    document.getElementById('customer_id').value = '';
-    document.getElementById('customer_name').value = '';
-    document.getElementById('customer_address').value = '';
-    document.getElementById('customer_contact').value = '';
-});
-
-// Click table row
+//------------------------- Click on Student Row ------------------------------
 $('#customer_table_body').on('click', 'tr', function () {
     let customer_obj = getCustomerDataByIndex($(this).index());
 
-    $('#customer_id').val(customer_obj.id);
-    $('#customer_name').val(customer_obj.name);
-    $('#customer_address').val(customer_obj.address);
-    $('#customer_contact').val(customer_obj.contact);
+    $('#customer_id_input').val(customer_obj.id);
+    $('#customer_name_input').val(customer_obj.name);
+    $('#customer_nic_input').val(customer_obj.nic);
+    $('#customer_phone_input').val(customer_obj.phone);
+    $('#customer_address_input').val(customer_obj.address);
 })
+
+//------------------------- Start: Student Add (Create) ------------------------------
+$('#customer_save_btn').on('click', function () {
+
+    let id = $('#customer_id_input').val();
+    let name = $('#customer_name_input').val();
+    let nic = $('#customer_nic_input').val();
+    let phone = $('#customer_phone_input').val();
+    let address = $('#customer_address_input').val();
+
+    (id === "") ? Swal.fire({ icon: "error", title: "Invalid ID!" }) :
+        (getCustomerDataById(id)) ? Swal.fire({ icon: "error", title: "ID already exists!" }) :
+            (name === "") ? Swal.fire({ icon: "error", title: "Invalid Name!" }) :
+                (!check_nic(nic)) ? Swal.fire({ icon: "error", title: "Invalid NIC!" }) :
+                    (!check_phone(phone)) ? Swal.fire({ icon: "error", title: "Invalid Phone!" }) :
+                        (address === "") ? Swal.fire({ icon: "error", title: "Invalid Address!" }) :
+                            (addCustomerData(id, name, nic, phone, address));
+    cleanCustomerForm();
+    Swal.fire({ icon: "success", title: "Customer saved successfully!"});
+    loadCustomerTbl();
+});
+//------------------------- End: Customer Add ------------------------------
+
+//------------------------- Start: Customer Update (Update) ------------------------------
+$('#customer_update_btn').on('click', function () {
+
+    let id = $('#customer_id_input').val();
+    let name = $('#customer_name_input').val();
+    let nic = $('#customer_nic_input').val();
+    let phone = $('#customer_phone_input').val();
+    let address = $('#customer_address_input').val();
+
+    (id === "") ? Swal.fire({ icon: "error", title: "Invalid ID!" }) :
+        (!getCustomerDataById(id)) ? Swal.fire({ icon: "error", title: "Customer not found!" }) :
+            (name === "") ? Swal.fire({ icon: "error", title: "Invalid Name!" }) :
+                (!check_nic(nic)) ? Swal.fire({ icon: "error", title: "Invalid NIC!" }) :
+                    (!check_phone(phone)) ? Swal.fire({ icon: "error", title: "Invalid Phone!" }) :
+                        (address === "") ? Swal.fire({ icon: "error", title: "Invalid Address!" }) :
+                            (updateCustomerData(id, name, nic, phone, address));
+    cleanCustomerForm();
+    Swal.fire({ icon: "success", title: "Customer updated successfully!" });
+    loadCustomerTbl();
+});
+//------------------------- End: Customer Update ------------------------------
+
+//------------------------- Start: Customer Delete (Delete) ------------------------------
+$('#customer_delete_btn').on('click', function () {
+
+    let id = $('#customer_id_input').val();
+
+    (id === "") ? Swal.fire({ icon: "error", title: "Invalid ID!" }) :
+        (!getCustomerDataById(id)) ? Swal.fire({ icon: "error", title: "Customer not found!" }) :
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Yes, delete it!"
+            })
+                .then((result) => {
+                    if (result.isConfirmed) {
+                        deleteCustomerData(id);
+                    }
+            })
+    cleanCustomerForm();
+    Swal.fire({ icon: "success", title: "Customer deleted successfully!" });
+    loadCustomerTbl();
+});
+
+//------------------------- End: Customer Delete ------------------------------
--- model/CustomerModel.js
+++ model/CustomerModel.js
@@ -1,37 +1,101 @@
+import {customer_db} from '../db/db.js';
+
 class Customer {
     #id;
     #name;
-    #contact;
+    #nic;
+    #phone;
     #address;
 
-    constructor(id, name,address, contact) {
+    constructor(id, name, nic, phone, address) {
         this.#id = id;
         this.#name = name;
-        this.#contact = contact;
+        this.#nic = nic;
+        this.#phone = phone;
         this.#address = address;
     }
+
     get id() {
         return this.#id;
     }
+
     get name() {
         return this.#name;
     }
-    get contact() {
-        return this.#contact;
+
+    get nic() {
+        return this.#nic;
     }
+
+    get phone() {
+        return this.#phone;
+    }
+
     get address() {
         return this.#address;
     }
+
     set id(id) {
         this.#id = id;
     }
+
     set name(name) {
         this.#name = name;
     }
-    set contact(contact) {
-        this.#contact = contact;
+
+    set nic(nic) {
+        this.#nic = nic;
     }
+
+    set phone(phone) {
+        this.#phone = phone;
+    }
+
     set address(address) {
         this.#address = address;
     }
 }
+
+// --------------------------- Add Customer ---------------------------
+const addCustomerData = (cid, cname, cnic, cphone, caddress) => {
+    let new_customer = new Customer(cid, cname, cnic, cphone, caddress);
+    customer_db.push(new_customer);
+}
+
+// --------------------------- Update Customer ---------------------------
+const updateCustomerData = (cid, cname, cnic, cphone, caddress) => {
+    let obj = customer_db.find(item => item.id == cid);
+
+    if(obj) {
+        obj.name=cname;
+        obj.nic=cnic;
+        obj.phone=cphone;
+        obj.address=caddress
+    }
+}
+
+// --------------------------- Delete Customer ---------------------------
+const deleteCustomerData = (cid) => {
+    let index = customer_db.findIndex(item => item.id == cid); // -1
+
+    if(index!==-1) {
+        customer_db.splice(index, 1);
+    }
+}
+
+// --------------------------- Get Customer ---------------------------
+const getCustomerData = () => {
+    return customer_db;
+}
+
+// --------------------------- Get Customer by Index ---------------------------
+const getCustomerDataByIndex = (index) => {
+    return customer_db[index];
+}
+
+// --------------------------- Get Customer by Id ---------------------------
+const getCustomerDataById = (id) => {
+    return customer_db.find(item => item.id==id);
+}
+
+export {addCustomerData, updateCustomerData, deleteCustomerData, getCustomerData, getCustomerDataByIndex, getCustomerDataById};
