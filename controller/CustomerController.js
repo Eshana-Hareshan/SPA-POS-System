fe288e01050ccfdf143bff0ce8781d8b9c408d29
Eshana-Hareshan <eshanahbandara@gmail.com>
2026-05-03 20:53:24 +0530

    create customer save function

---
+++ controller/CustomerController.js
@@ -0,0 +1,28 @@
+document.getElementById('save_customer_btn').addEventListener('click', function () {
+
+    const id = document.getElementById('cust_id').value;
+    const name = document.getElementById('cust_name').value;
+    const address = document.getElementById('cust_address').value;
+    const contact = document.getElementById('cust_contact').value;
+
+    if (!id || !name || !address || !contact) {
+        alert("Please fill all fields");
+        return;
+    }
+
+    const newCustomer = `
+        <tr>
+            <td>${id}</td>
+            <td>${name}</td>
+            <td>${address}</td>
+            <td>${contact}</td>
+        </tr>
+    `;
+
+    document.getElementById('customer_table_body').innerHTML += newCustomer;
+
+    document.getElementById('cust_id').value = '';
+    document.getElementById('cust_name').value = '';
+    document.getElementById('cust_address').value = '';
+    document.getElementById('cust_contact').value = '';
+});
--- controller/SideBarController.js
+++ controller/SideBarController.js
@@ -1,32 +1,8 @@
-// // Nav Bar toggler
-// const navbarToggle = document.querySelector('.navbar-toggler');
-//
-// navbarToggle.addEventListener('click', () => {
-//     navbarToggle.classList.toggle('active');
-// });
-//
-// // Side Bar
-// let sidebar_open = true;
-//
-// $('#menu_btn').on('click', function () {
-//     if(sidebar_open) {
-//         $('#sidebar').css({display: 'block'});
-//         $('.content').css({marginLeft: '250px'});
-//         sidebar_open = false;
-//     } else {
-//         $('#sidebar').css({display: 'none'});
-//         $('.content').css({marginLeft: '0px'});
-//         sidebar_open = true;
-//
-//     }
-// })
-
-// Nav Bar toggler + Sidebar together
-let sidebar_open = false; // ✅ start CLOSED
+// Nav Bar toggler + Sidebar
+let sidebar_open = false;
 
 $('#menu_btn').on('click', function () {
 
-    // hamburger animation
     $(this).toggleClass('active');
 
     if (!sidebar_open) {
