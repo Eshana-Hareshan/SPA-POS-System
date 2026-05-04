68f30d3e0a391451d7d55729750788e0903dec22
Eshana-Hareshan <eshanahbandara@gmail.com>
2026-05-04 12:04:56 +0530

    create order DB

--- db/db.js
+++ db/db.js
@@ -4,4 +4,6 @@ let customer_db = [];
 // ------------------------ Item DB ------------------------------
 let item_db = [];
 
-export {customer_db, item_db};
+let order_db = [];
+
+export { customer_db, item_db, order_db };
